750748e9b0dd2e08ab4ef99395fdaa09ab4f4e22
masafumi-nemoto <auther.masahito@gmail.com>
2020-03-01 23:22:33 +0900

    投稿機能を作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200301054601) do
+ActiveRecord::Schema.define(version: 20200301111546) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "user_id"
+    t.string   "title"
+    t.string   "description"
+    t.string   "image"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
